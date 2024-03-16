f629654eb6c3b19c433bbabaadb0cd6a5072e282
Homan Mirgolbabaee <homanmirgolbabaee@gmail.com>
2024-03-16 09:47:56 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,15 +2,16 @@ import datetime
 import streamlit as st
 from PIL import Image
 from fpdf import FPDF
+
 # Assuming generate_prediction and summarize_pdf are defined in predictor.py
-from predictor import generate_prediction
+from predictor import generate_prediction , summarize_pdf
 import time
 from streamlit_lightweight_charts import renderLightweightCharts
 import yfinance as yf
 import pandas as pd
 import  streamlit_toggle as tog
 
-from streamlit_telegram_login import TelegramLoginWidgetComponent
+from streamlit_telegram_login import TelegramLoginWidgetComponent 
 from streamlit_telegram_login.helpers import YamlConfig
 from streamlit_lottie import st_lottie
 from streamlit_lottie import st_lottie_spinner
@@ -121,9 +122,9 @@ def price_prediction():
         prompt = st.text_input("Enter a prompt for the prediction")
 
 
-    if st.button("Download"):
+    if st.button("Submit"):
         with st_lottie_spinner(lottie_json, key="download" , width=100):
-            time.sleep(5)
+            time.sleep(2.5)
         st.balloons()                
                      
     with download_report_col:
@@ -163,12 +164,73 @@ def price_prediction():
                         mime="application/octet-stream"
                     )
                 st.success("Report generated successfully. Please download using the button above.")
-        else:
-            st.warning("Please upload a chart image and enter a prompt to enable report generation.")
+                st.select_slider('Word Limit', options=[20, 50, 100, 400, 500])
+                if st.checkbox("Summarized"):
+                    result = summarize_pdf("reports/prediction_report.pdf")
+                
+                    if result and len(response) > 0 and result[0].type == 'text':
+                        prediction_text = result[0].text
+                        
+                        # Generate PDF report
+                        pdf = FPDF()
+                        pdf.add_page()
+                        pdf.set_font("Helvetica", 'B', 16)
+                        pdf.cell(0, 10, 'Cryptocurrency Prediction Report', 0, 1, 'C')
+                        pdf.set_y(pdf.get_y() + 20)
+                        pdf.set_font("Helvetica", size=12)
+                        #now = datetime.now()
+                        #date_string = now.strftime("%B %d, %Y")
+                        #pdf.cell(0, 10, f'Report Generated on: {date_string}', 0, 1, 'C')
+                        pdf.add_page()
+                        pdf.set_font("Helvetica", 'B', 14)
+                        pdf.set_y(pdf.get_y() + 10)
+                        pdf.set_font("Helvetica", size=12)
+                        pdf.multi_cell(0, 10, prediction_text)
+                        filename = f"reports/summarized_prediction_report.pdf"
+                        pdf.output(filename)
+                        
+                        # Provide a link for the user to download the report
+                        with open(filename, "rb") as file:
+                            btn = st.download_button(
+                                label="Download Prediction Report",
+                                data=file,
+                                file_name=filename,
+                                mime="application/octet-stream"
+                            )                
+                    
+            else:
+                st.warning("Please upload a chart image and enter a prompt to enable report generation.")
+        
+            
 
 
 def telgeram_streamlit_app():
-    st.title("under construction 🚧")
+    st.title("Under Construction 🚧")
+    
+    # Assuming your 'config.yaml' is in the same directory as your script
+    #config_file = "config.yaml"
+    
+    # Load the configuration
+    #config = YamlConfig(config_file)
+    #telegram_login = TelegramLoginWidgetComponent(**config.config)
+    
+    # Display the Telegram login button and get the user data upon successful login
+    #user_data = telegram_login.button
+    #if user_data:
+    #    st.write("Welcome, ", user_data["first_name"])
+        
+        # Display a button to send a report after successful login
+    #    if st.button("Send Report"):
+            # Placeholder report message
+    #        report_message = "Here's your special report from our Streamlit App!"
+            
+            # Send the report to the user via your Telegram bot
+            # Assuming you have a mechanism to obtain the user's Telegram ID from `user_data`
+            #send_report_to_user(user_data["id"], report_message)
+            
+    #        st.success("Report sent successfully to your Telegram!")
+    #else:
+    #    st.write("Please log in using Telegram.")
 
 st.set_page_config(
     page_title="Claude Crypto Assistant",
@@ -180,10 +242,6 @@ st.set_page_config(
         'About': "# This is a header. This is an *extremely* cool app!"
     }
 )
-config_file = "config.yaml"
-# Load the configuration
-config = YamlConfig(config_file)
-telegram_login = TelegramLoginWidgetComponent(**config.config)
 
 
 
@@ -191,14 +249,9 @@ def main():
     st.sidebar.title("🚀 Navigation")
     st.sidebar.markdown("Explore the different functionalities of Claude Crypto Assistant.")
     page = st.sidebar.radio("Go to", ["Home 🏠", "Price Predictor 🔮","Visualize Data 📈","Telegram"])
-    tog.st_toggle_switch(label="Label", 
-                    key="Key1", 
-                    default_value=False, 
-                    label_after = False, 
-                    inactive_color = '#D3D3D3', 
-                    active_color="#11567f", 
-                    track_color="#29B5E8"
-                    )
+
+    
+
     if page == "Home 🏠":
         
         
